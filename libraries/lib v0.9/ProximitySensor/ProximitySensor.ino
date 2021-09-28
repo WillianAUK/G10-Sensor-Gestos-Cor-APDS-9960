907ef0803b20bdc9206438407bd0c8a80e5b5153
Willian-AUK <willian.adriano@academico.ifpb.edu.br>
2021-09-27 22:15:09 -0300

    adicionando arquivos lib em produção

---
+++ libraries/lib v0.9/ProximitySensor/ProximitySensor.ino
@@ -0,0 +1,70 @@
+/****************************************************************
+
+
+Tests the proximity sensing abilities of the APDS-9960.
+Configures the APDS-9960 over I2C and polls for the distance to
+the object nearest the sensor.
+
+Hardware Connections:
+
+IMPORTANT: The APDS-9960 can only accept 3.3V!
+ 
+ Arduino Pin  APDS-9960 Board  Function
+ 
+ 3.3V         VCC              Power
+ GND          GND              Ground
+ A4           SDA              I2C Data
+ A5           SCL              I2C Clock
+
+
+****************************************************************/
+
+#include <Wire.h>
+#include <SparkFun_APDS9960.h>
+
+// Global Variables
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
+uint8_t proximity_data = 0;
+
+void setup() {
+  
+  // Initialize Serial port
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("SparkFun APDS-9960 - ProximitySensor"));
+  Serial.println(F("------------------------------------"));
+  
+  // Initialize APDS-9960 (configure I2C and initial values)
+  if ( apds.init() ) {
+    Serial.println(F("APDS-9960 initialization complete"));
+  } else {
+    Serial.println(F("Something went wrong during APDS-9960 init!"));
+  }
+  
+  // Adjust the Proximity sensor gain
+  if ( !apds.setProximityGain(PGAIN_2X) ) {
+    Serial.println(F("Something went wrong trying to set PGAIN"));
+  }
+  
+  // Start running the APDS-9960 proximity sensor (no interrupts)
+  if ( apds.enableProximitySensor(false) ) {
+    Serial.println(F("Proximity sensor is now running"));
+  } else {
+    Serial.println(F("Something went wrong during sensor init!"));
+  }
+}
+
+void loop() {
+  
+  // Read the proximity value
+  if ( !apds.readProximity(proximity_data) ) {
+    Serial.println("Error reading proximity value");
+  } else {
+    Serial.print("Proximity: ");
+    Serial.println(proximity_data);
+  }
+  
+  // Wait 250 ms before next reading
+  delay(250);
+}
